ee8509298b7438a631a426654568469414a827be
AidenCastillo <amc-mcjarjar@hotmail.com>
2022-04-24 22:24:12 -0500

    finally added stuff

--- cog.py
+++ cog.py
@@ -1,8 +1,1 @@
-import random
-
-def flip(p):
-    p = float(p)
-    if p.is_integer():
-        p = p / 10
-        print(p)
-    return (random.random() < p)
+version = "basic"
--- main.py
+++ main.py
@@ -1,35 +1,94 @@
 import os
+import sys
 os.chdir(os.getcwd())
 
 import json
 import random
 from cog import *
 
-version = "basic"
-
 
 with open("variables/settings.json") as f:
     settings = json.loads(f.read())
 with open("variables/archetype.json") as f:
     archeType = json.loads(f.read())
+    print(archeType)
     archeNames = []
     for x in archeType:
         archeNames.append(x)
 with open("variables/ride.json") as f:
-    ridesList = json.loads(f.read())
+    rides = json.loads(f.read())
     rideNames = []
-    for x in ridesList:
+    for x in rides:
         rideNames.append(x)
 
 class person:
-    def __init__(self, type, location):
-        self.type = type
+    def __init__(self, arche, location, CurrentWait):
+        self.arche = arche
+        self.location = location
+        self.CurrentWait = CurrentWait
 
-agents = list()
-for i in range(settings[version]['population']):
-    agent = person(random.choice(archeNames), "Hub")
-    agents.append(agent)
+class ParkSim:
+    def __init__(self):
+        '''
+        Seed: Randomizer seed
 
-for x in agents:
-    ride = random.choice(rideNames)
+        Time: Runs on military time for simplicity
+        '''
+        self.seed = settings[version]['seed']
+        self.time = 8
+        self.agents = list()
+
+    def runSim(self):
+        self.CreateAgents()
+        print(self.agents)
+        while True:
+            for i in self.agents:
+                
+                if i.location == "Hub":
+                    self.weightedChoice(i, "ride")
+                    print("here1")
+                elif i.location in rideNames:
+                    print("here2")
+                    if i.CurrentWait == archeType[i.arche]["wait"]:
+                        self.weightedChoice(i, "ride")
+                        print("here4")
+                        i.CurrentWait = 0
+
+            self.timeChange()
+            self.log
+            
     
+    def CreateAgents(self):
+        for i in range(settings[version]['population']):
+            agent = person(random.choice(archeNames), "Hub", 0)
+            self.agents.append(agent)
+
+    def weightedChoice(self, target, type):
+        weight = list()
+        if  type == "ride":
+            for x in rideNames:
+                weight.append(rides[x]["popularity"])
+                
+            target.location = random.choices(rideNames, weights=weight)[0]
+    def timeChange(self):
+        if self.time <= 22:
+            self.time += 1
+            for i in self.agents:
+                i.CurrentWait += 1
+        else:
+            sys.exit()
+    def log(self, data, file):
+        with open(file) as f:
+            current = json.loads(f.read())
+        current.append(data)
+        with open(file, "w") as f:
+            f.write(json.dumps(current))
+def main():
+    simulation = ParkSim()
+    simulation.runSim()
+
+            
+
+#start sim
+if __name__ == '__main__':
+    main()
